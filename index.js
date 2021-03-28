37cd8d86d6a36f26e06cd2645958cef01fcb3069
Jerameel <jerameeldelosreyes@gmail.com>
2021-03-28 10:47:10 +0800

    Fix: hide yellow warnings

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './src/App';
 import Storybook from './storybook';
 import { name as appName } from './app.json';
-
+LogBox.ignoreAllLogs(true);
 AppRegistry.registerComponent(appName, () => App);
