bc82ceaa02459ac7893f2db6305cbc4d1cf87104
Jerameel <jerameeldelosreyes@gmail.com>
2021-04-03 11:53:26 +0800

    Update: rename application to bacon

--- android/app/src/main/java/com/jerameeldelosreyes/bacon/MainActivity.java
+++ android/app/src/main/java/com/jerameeldelosreyes/bacon/MainActivity.java
@@ -1,4 +1,4 @@
-package com.transmitr;
+package com.jerameeldelosreyes.bacon;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "transmitr";
+    return "bacon";
   }
 }
